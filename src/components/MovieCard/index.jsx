ecfbf54e5c03ab47149a1eb98ca610c1398e9fd1
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2024-12-13 15:58:34 +0200

    Error handle for image loading

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./style.css";
 
-const MovieCard = ({ imageSrc, imageAlt, imageTitle, title, genre, rating }) => {
+const MovieCard = ({
+  imageSrc,
+  imageAlt,
+  imageTitle,
+  title,
+  genre,
+  rating,
+}) => {
+  const handleError = (e) => {
+    e.target.src = "assets/default.jpg";
+  };
   return (
     <div className="movie__card">
-      <img src={`assets/${imageSrc}`} alt={imageAlt} title={imageTitle} />
+      <img
+        src={`assets/${imageSrc}`}
+        alt={imageAlt}
+        title={imageTitle}
+        onError={handleError}
+      />
       <div className="movie__card__info">
         <h3 className="movie__card__title">{title}</h3>
         <p className="movie__card__genre">{genre}</p>
